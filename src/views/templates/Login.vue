6352e4f472cbaf2fbe1da9c6a270feba3c8e67be
yu_5 <yu-five@user.noreply.gitee.com>
2021-05-20 19:49:26 +0800

    菜单界面

--- src/views/templates/Login.vue
+++ src/views/templates/Login.vue
@@ -1,110 +1,116 @@
 <template>
     <div class="login-wrap">
+        <div class="ms-title">每日一报系统</div>
+        <div class="ms-content">浙江工业大学<br>防疫期间<br>签到销假系统</div>
         <div class="ms-login">
-            <div class="ms-title">后台管理系统</div>
-            <el-form :model="param" :rules="rules" ref="login" label-width="0px" class="ms-content">
+            <el-form :model="param" :rules="rules" ref="login" label-width="30px" class="ms-content" >
+
                 <el-form-item prop="username">
                     <el-input v-model="param.username" placeholder="username">
                         <el-button slot="prepend" icon="el-icon-lx-people"></el-button>
                     </el-input>
                 </el-form-item>
+
                 <el-form-item prop="password">
                     <el-input
-                        type="password"
-                        placeholder="password"
-                        v-model="param.password"
-                        @keyup.enter.native="submitForm()">
+                            type="password"
+                            placeholder="password"
+                            v-model="param.password"
+                            @keyup.enter.native="submitForm()">
                         <el-button slot="prepend" icon="el-icon-lx-lock"></el-button>
                     </el-input>
                 </el-form-item>
+
+                <el-radio-group v-model="radio">
+                    <el-radio :label="1">学生</el-radio>
+                    <el-radio :label="2">教职工</el-radio>
+                    <el-radio :label="3">管理员</el-radio>
+                </el-radio-group>
+
+
+
                 <div class="login-btn">
                     <el-button type="primary" @click="submitForm()">登录</el-button>
                 </div>
-                <p class="login-tips">Tips : 用户名和密码随便填。</p>
             </el-form>
+
+
         </div>
     </div>
 </template>
-
 <script>
-export default {
-  inject:['routerRefresh'],   //在子组件中注入在父组件中创建的属性
-    data: function() {
-        return {
-            param: {
-                username: 'admin',
-                password: '123123',
-            },
-            rules: {
-                username: [{ required: true, message: '请输入用户名', trigger: 'blur' }],
-                password: [{ required: true, message: '请输入密码', trigger: 'blur' }],
-            },
-        };
-    },
-  watch: {
-    // eslint-disable-next-line no-unused-vars
-    '$route' (to, from) {
-      this.$router.go(0);
-    }
-  },
-    methods: {
-        submitForm() {
-            this.$refs.login.validate(valid => {
-                if (valid) {
-                    this.$message.success('登录成功');
-                    localStorage.setItem('ms_username', this.param.username);
-                  this.$router.replace('/');
-                } else {
-                    this.$message.error('请输入账号和密码');
-                    console.log('error submit!!');
-                    return false;
-                }
-            });
+    export default {
+        name: "Login",
+        data: function() {
+            return {
+                param: {
+                    username: '',
+                    password: '',
+                },
+                rules: {
+                    username: [{ required: true, message: '请输入用户名', trigger: 'blur' }],
+                    password: [{ required: true, message: '请输入密码', trigger: 'blur' }],
+                },
+            };
         },
-    },
-};
+        methods: {
+            submitForm() {
+                this.$refs.login.validate(valid => {
+                    if (valid) {
+                        this.$message.success('登录成功');
+                        localStorage.setItem('ms_username', this.param.username);
+                        this.$router.push('Menu');
+
+                    } else {
+                        this.$message.error('请输入账号和密码');
+                        console.log('error submit!!');
+                        return false;
+                    }
+                });
+            },
+        },
+    }
 </script>
 
 <style scoped>
-.login-wrap {
-    position: relative;
-    width: 100%;
-    height: 100%;
-    background-image: url(../../assets/img/login-bg.jpg);
-    background-size: 100%;
-}
-.ms-title {
-    width: 100%;
-    line-height: 50px;
-    text-align: center;
-    font-size: 20px;
-    color: #fff;
-    border-bottom: 1px solid #ddd;
-}
-.ms-login {
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    width: 350px;
-    margin: -190px 0 0 -175px;
-    border-radius: 5px;
-    background: rgba(255, 255, 255, 0.3);
-    overflow: hidden;
-}
-.ms-content {
-    padding: 30px 30px;
-}
-.login-btn {
-    text-align: center;
-}
-.login-btn button {
-    width: 100%;
-    height: 36px;
-    margin-bottom: 10px;
-}
-.login-tips {
-    font-size: 12px;
-    line-height: 30px;
-    color: #fff;
-}
+    .login-wrap {
+        position: relative;
+        width: 100%;
+        height: 1000px;
+        background:url("src/assets/img/login-bg.jpg");
+        background-size: 100%;
+    }
+    .ms-title {
+        width: 100%;
+        line-height: 50px;
+        text-align: center;
+        font-size: 20px;
+        border-bottom: 1px solid #ddd;
+    }
+    .ms-login {
+        margin: 0 auto;
+        /*text-align: center;*/
+        width: 400px;
+        /*margin: -190px 0 0 -175px;*/
+        border-radius: 5px;
+        background: rgba(255, 255, 255, 0.3);
+
+    }
+    .ms-content {
+        padding: 30px 30px;
+        font-size: 30px;
+    }
+    .login-btn {
+        text-align: center;
+    }
+    .login-btn button {
+        width: 100%;
+        height: 36px;
+        margin-bottom: 10px;
+    }
+    .login-tips {
+        font-size: 12px;
+        line-height: 30px;
+        color: #fff;
+    }
 </style>
