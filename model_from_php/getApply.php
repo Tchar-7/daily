b0004ed0970ce31147895e3fb1d9659682d01bfd
shh <2421250544@qq.com>
2021-05-22 11:07:22 +0800

    menu页面显示内容的实现

--- model_from_php/getApply.php
+++ model_from_php/getApply.php
@@ -5,12 +5,14 @@ mysqli_query($conn, "SET NAMES utf8");
 if($conn->connect_error){
     die("数据库连接失败".mysqli_connect_error());
 }
+
 $ID = $_POST['userID'];
+$now=date("Y-m-d H:i:s",time());
+$firstDay = date('Y-m-01 00:00:00');
 
 $user = array();
 
-$sql = "select * from personal_info where ID = '$ID'";
-
+$sql = "select * from personal_info where ID = '$ID' and (time >'$firstDay'and time <'$now')" ;
 $result = $conn->query($sql);
 
 if(mysqli_num_rows($result)>0){
@@ -21,3 +23,4 @@ if(mysqli_num_rows($result)>0){
 
 $conn->close();
 ?>
+
