6879b867cd426ea47bf396e825a68642b4b4ab38
shh <2421250544@qq.com>
2021-05-23 14:48:27 +0800

    一些后台文件的更新

--- model_from_php/TeacherChuxiao.php
+++ model_from_php/TeacherChuxiao.php
@@ -20,8 +20,18 @@ $time=date("Y-m-d H:i:s",time());
 for($i=0;$i<$num;$i++){
     $sql="insert into teacher_out values('$name','$userID','$sex','$phone','$address[$i]','$start[$i]','$finish[$i]','$time')";
     $result = $conn->query($sql);
+    $sql1="select max(ID) from teacher_out";
+    $result1 = $conn->query($sql1);
+    if($result1->num_rows>0) {
+        while ($row = $result1->fetch_assoc()) {
+            $ID=$row['ID'];
+        }
+    }
+    $ID='TeaOut'.$ID;
+    $sql2="insert into apply_recording values('$ID','$userID','出差','$time',0,'')";
+    $result2 = $conn->query($sql2);
 }
-$sql0="insert into apply_recording values(recordID,'$userID','出差','$time',2,'')";
+
 
 $conn->close();
 ?>
